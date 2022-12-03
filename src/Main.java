53fbfd74fa94066fec0d2e85c22ea71bac390861
Alex <loalantref@hmail.com>
2022-12-03 17:59:45 +1200

    Начали

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        int [] firstMass = new int[3];
+        firstMass[0] = 1;
+        firstMass[1] = 2;
+        firstMass[2] = 3;
+        float [] secondMass = {(float) 1.57, (float) 7.654, (float) 9.986};
+        int [] thirdtMass = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        System.out.println("/////////////////////////////////////////////////////////////////////////////////");
+        System.out.println(firstMass[0] + " ," + firstMass[1] + " ," + firstMass[2]);
+        System.out.println(secondMass[0] + " ," + secondMass[1] + " ," + secondMass[2]);
+
+
+
+
+
+
+
+
+
+        System.out.println("Hello world!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
