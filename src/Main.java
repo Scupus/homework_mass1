f18d88bfdae8231b3fa00ec7cb97a8392099ebb7
Alex <loalantref@hmail.com>
2022-12-03 19:08:31 +1200

    Продолжаем продолжать

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         int [] firstMass = new int[3];
@@ -5,10 +8,30 @@ public class Main {
         firstMass[1] = 2;
         firstMass[2] = 3;
         float [] secondMass = {(float) 1.57, (float) 7.654, (float) 9.986};
-        int [] thirdtMass = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        int [] thirdMass = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
         System.out.println("/////////////////////////////////////////////////////////////////////////////////");
-        System.out.println(firstMass[0] + " ," + firstMass[1] + " ," + firstMass[2]);
-        System.out.println(secondMass[0] + " ," + secondMass[1] + " ," + secondMass[2]);
+        System.out.println(firstMass[0] + ", " + firstMass[1] + ", " + firstMass[2]);
+        System.out.println(secondMass[0] + ", " + secondMass[1] + ", " + secondMass[2]);
+        for (int i = 0; i < 11; i++) {
+            System.out.print(thirdMass[i] + ", ");
+        }
+            System.out.println(thirdMass[11]);
+            System.out.println("/////////////////////////////////////////////////////////////////////////////////");
+            System.out.println(firstMass[2] + ", " + firstMass[1] + ", " + firstMass[0]);
+            System.out.println(secondMass[2] + ", " + secondMass[1] + ", " + secondMass[0]);
+            for (int i = 11; i > 0; i--) {
+                System.out.print(thirdMass[i] + ", ");
+            }
+            System.out.println(thirdMass[0]);
+            System.out.println("/////////////////////////////////////////////////////////////////////////////////");
+                for (int f = 0; f < 3; f++) {
+                if (firstMass[f] % 2 != 0){
+                    firstMass[f] += 1;
+                    System.out.println(f);
+                }
+
+
+            }
 
 
 
@@ -18,7 +41,7 @@ public class Main {
 
 
 
-        System.out.println("Hello world!");
+            System.out.println("Hello world!");
 
 
 
